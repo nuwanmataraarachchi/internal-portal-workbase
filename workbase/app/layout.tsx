35e1d237f03248f9c6c7c9cb51f8fca5cf0f7a58
Nuwan Mataraarachchi <88268451+nuwanmataraarachchi@users.noreply.github.com>
2026-09-16 18:55:09 +0530

    added the profile and ui ux fixes

--- workbase/app/layout.tsx
+++ workbase/app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <AppShell role={session?.role}>{children}</AppShell>
+        <AppShell session={session}>{children}</AppShell>
       </body>
     </html>
   );
